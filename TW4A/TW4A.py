5f79cf6b13ee9822c63d38f2fd7bcf4362489007
Aniket Saha <anikets349@gmail.com>
2022-08-02 21:44:45 +0530

    Update TW4A

--- TW4A/TW4A.py
+++ TW4A/TW4A.py
@@ -1,54 +1,38 @@
-import  numpy as np
-
-x = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
-print('Input:')
-print(x)
-
-y = np.array([[0], [1], [1], [0]])
-print('Actual output: ')
-print(y)
+import numpy as np
 
 def sigmoid(x):
-	return 1 / (1 + np.exp(-x))
+    return 1 / (1 + np.exp(-x))
 
-def derivatives_sigmoid(x):
-	return x * (1 - x)
+def derivative_sigmoid(x):
+    return x * (1 - x)
 
-# initializing variables
-epoch = 6000
-lr = 0.15
-inputlayer_neurons = x.shape[1]
-hiddenlayer_neurons = 2
-output_neurons = 1
+inputs = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+outputs = np.array([[0], [1], [1], [0]])
+inputLayerNeurons, hiddenLayerNeurons, outputLayerNeurons = 2, 2, 1
+a = 0.1
+epochs = 10000
 
-# initializing weight and bias
-wh = np.random.uniform(size=(inputlayer_neurons, hiddenlayer_neurons))
-bh = np.random.uniform(size=(1, hiddenlayer_neurons))
-wout = np.random.uniform(size=(hiddenlayer_neurons, output_neurons))
-bout = np.random.uniform(size=(1, output_neurons))
+wh = np.random.uniform(size=(inputLayerNeurons, hiddenLayerNeurons))
+bh = np.random.uniform(size=(1, hiddenLayerNeurons))
+wout = np.random.uniform(size=(hiddenLayerNeurons, outputLayerNeurons))
+bout = np.random.uniform(size=(1, outputLayerNeurons))
 
-# training the model
-for i in range(epoch):
-	# forward propagation
-	hidden_layer_input1 = np.dot(x, wh)
-	hidden_layer_input = hidden_layer_input1 + bh
-	hiddenlayer_activations = sigmoid(hidden_layer_input)
-	output_layer_input1 = np.dot(hiddenlayer_activations, wout)
-	output_layer_input = output_layer_input1 + bout
-	output = sigmoid(output_layer_input)
+for i in range(epochs):
+    # forward propagation
+    hidden_layer_input = np.dot(inputs, wh) + bh
+    hidden_layer_output = sigmoid(hidden_layer_input)
+    output_layer_input = np.dot(hidden_layer_output, wout) + bout
+    pred_output = sigmoid(output_layer_input)
+    # backward propagation
+    e = outputs - pred_output
+    d_output = e * derivative_sigmoid(pred_output)
+    error_hidden_layer = np.dot(d_output, wout.T)
+    d_hidden = error_hidden_layer * derivative_sigmoid(hidden_layer_output)
+    wout += np.dot(hidden_layer_output.T, d_output) * a
+    bout += np.sum(d_output, axis=0, keepdims=True) * a
+    wh += np.dot(inputs.T, d_hidden) * a
+    bh += np.sum(d_hidden, axis=0, keepdims=True) * a
 
-	# backpropagation
-	e = y - output
-	slope_output_layer = derivatives_sigmoid(output)
-	slope_hidden_layer = derivatives_sigmoid(hiddenlayer_activations)
-	d_output = e * slope_output_layer
-	error_at_hidden_layer = d_output.dot(wout.T)
-	d_hiddenlayer = error_at_hidden_layer * slope_hidden_layer
-	wout += hiddenlayer_activations.T.dot(d_output) * lr
-	bout += np.sum(d_output, axis = 0, keepdims = True) * lr
-	wh += x.T.dot(d_hiddenlayer) * lr
-	bh += np.sum(d_hiddenlayer, axis = 0, keepdims = True) * lr
-
-print('Output from the model: ')
-print(output)
-print(np.round(output))
+print("Predicted outputs: ")
+print(pred_output)
+print(np.round(pred_output))
